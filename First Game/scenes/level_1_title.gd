90cc70dd2aef757b5460c4125f06df0a9613101a
Roxanne Seaton <help@roxannecreativeinc.com>
2026-02-05 18:51:47 -0800

    level arrangement and ui elements

---
+++ First Game/scenes/level_1_title.gd
@@ -0,0 +1,15 @@
+extends Control
+
+@onready var lvl1play_button = %Lvl1play
+@onready var back_button = %BackButton
+
+func _ready():
+	lvl1play_button.pressed.connect(_on_lvl1play_pressed)
+	back_button.pressed.connect(_on_back_pressed)
+	lvl1play_button.grab_focus()
+
+func _on_lvl1play_pressed():
+	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
+
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://scenes/start_menu.tscn")
---
+++ First Game/scenes/level_select.gd
@@ -0,0 +1,16 @@
+extends Control
+
+@onready var lvl1 = %Lvl1
+@onready var back = %Back
+
+func _ready():
+	lvl1.pressed.connect(_on_lvl1_pressed)
+	back.pressed.connect(_on_back_pressed)
+	lvl1.grab_focus()
+	
+func _on_lvl1_pressed():
+	get_tree().change_scene_to_file("res://scenes/level1-title.tscn")
+
+
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://scenes/start_menu.tscn")
