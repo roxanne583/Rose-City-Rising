427e5baa8f1498932dd1d4df753d628df4bfd97a
Roxanne Seaton <help@roxannecreativeinc.com>
2026-04-30 20:55:21 -0700

    Error fixes
    Adjusted the UI dialog errors

--- First Game/scripts/quest_giver.gd
+++ First Game/scripts/quest_giver.gd
@@ -1,17 +1,10 @@
 extends Area2D
 
+@export var intro_text := "We need to pickup \"Playdates\"..."
 
-@export var intro_text := "We need to pickup \"Playdates\" from \"Panic!\" in downtown. Let's skate"
+@export var reminder_text := "Is everything good pal?"
 
-@export var reminder_text := "Is everything good pal? You should hurry and pickup the \"Playdate\" before p:ear closes."
-
-@export var completion_text := "You rock! Wired Wednesday will be happy. Let's see what p:ear's skate club is up to."
-
-@onready var dialogue_panel = $CanvasLayer/PanelContainer
-
-@onready var dialogue_label = $CanvasLayer/PanelContainer/Label
-
-@onready var dialogue_button = $CanvasLayer/PanelContainer/Button
+@export var completion_text := "You rock!"
 
 var player: Node = null
 
@@ -23,15 +16,11 @@ func _ready():
 
 	body_entered.connect(_on_body_entered)
 	body_exited.connect(_on_body_exited)
-	dialogue_panel.visible = false
-	dialogue_button.pressed.connect(_on_dialogue_confirmed)
 	_show_intro()
 
 func _process(_delta):
 
 	if not player:
-		return
-	if dialogue_panel.visible:
 		return
 	if Input.is_action_just_pressed("interact"):
 		_show_interaction_dialogue()
@@ -51,22 +40,19 @@ func _show_intro():
 	if intro_shown:
 		return
 	intro_shown = true
-	dialogue_label.text = intro_text
-	dialogue_panel.visible = true
+	DialogueManager.show(intro_text)
 
 func _show_interaction_dialogue():
 
 	if player and player.has_playdate:
 		completion_pending = true
-		dialogue_label.text = completion_text
+		DialogueManager.show(completion_text, _on_dialogue_done)
 	else:
 		completion_pending = false
-		dialogue_label.text = reminder_text
-	dialogue_panel.visible = true
+		DialogueManager.show(reminder_text)
 
-func _on_dialogue_confirmed():
+func _on_dialogue_done():
 
-	dialogue_panel.visible = false
 	if completion_pending:
 		completion_pending = false
 		GameManager.trigger_victory()
